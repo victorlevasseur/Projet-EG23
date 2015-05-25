76f5fd87c45cc793feee30a9438feb1c9b78840f
Victor Levasseur <victorlevasseur52@gmail.com>
2015-05-25 17:05:31 +0200

    Ajout des informations sur le nbr de calories et le tps total dans le contrôle d'affichage de repas (TPlanningDayControl)

--- src/planningday.pas
+++ src/planningday.pas
@@ -6,27 +6,58 @@ interface
 
 uses
   Classes, SysUtils, db, dbf, sqldb, FileUtil, Forms, Controls, StdCtrls,
-  ComCtrls, Grids, ColorBox, ValEdit, Dialogs, DBGrids;
+  ComCtrls, Grids, Dialogs;
 
 type
 
   { TPlanningDayControl }
-
+  //Cette classe est un contrôle permettant d'afficher le contenu d'un repas
+  //sur le planning (entrée, plat et dessert) ainsi que quelques informations
+  //comme le temps de préparation, le nombre de calories...
   TPlanningDayControl = class(TFrame)
     Datasource1: TDatasource;
+    DurationLabel: TLabel;
     RecipeDbf: TDbf;
-    InformationLabel: TLabel;
+    CaloriesLabel: TLabel;
     ListView1: TListView;
     procedure RecipeDbfAfterOpen(DataSet: TDataSet);
   private
     { private declarations }
+
+    //Fonction permettant de récupérer le nom d'une recette depuis son CODE/ID
     function GetRecipeName(Id: Integer) : string;
+
+    //Met à jour l'affichage de la liste des recettes du repas ainsi que
+    //le informations sur le temps de préparation / calories
+    procedure UpdateListView();
+  private
+    //Stockage des IDs correspondant aux plats
+    StarterId: Integer;
+    MealId: Integer;
+    DessertId: Integer;
   public
     { public declarations }
+
+    //Surcharge du constructeur permettant d'initialiser la connexion à la base
+    //de données.
     constructor Create(AOwner: TComponent) ; override;
 
+    //Permet de récupérer l'ID de l'entrée
+    function GetStarter : Integer;
+
+    //Permet de définir l'entrée du repas (son ID)
     procedure SetStarter(Id: Integer);
+
+    //Permet de récupérer l'ID du repas
+    function GetMeal : Integer;
+
+    //Permet de définir le plat du repas (son ID)
     procedure SetMeal(Id: Integer);
+
+    //Permet de récupérer l'ID du dessert
+    function GetDessert : Integer;
+
+    //Permet de définir le dessert du repas (son ID)
     procedure SetDessert(Id: Integer);
   end;
 
@@ -49,9 +80,43 @@ begin
      GetRecipeName := RecipeDbf.FieldByName('INTITULE').AsString;
 end;
 
+procedure TPlanningDayControl.UpdateListView;
+var
+  totalCalories: Integer;
+  totalDuration: Integer;
+begin
+     ListView1.Items.Item[0].Caption := GetRecipeName(StarterId);
+     ListView1.Items.Item[1].Caption := GetRecipeName(MealId);
+     ListView1.Items.Item[2].Caption := GetRecipeName(DessertId);
+
+     //Calcul du total de calories
+     RecipeDbf.Locate('CODE', StarterId, [loCaseInsensitive]);
+     totalCalories := RecipeDbf.FieldByName('CALORIE').AsInteger;
+     RecipeDbf.Locate('CODE', MealId, [loCaseInsensitive]);
+     totalCalories := totalCalories + RecipeDbf.FieldByName('CALORIE').AsInteger;
+     RecipeDbf.Locate('CODE', DessertId, [loCaseInsensitive]);
+     totalCalories := totalCalories + RecipeDbf.FieldByName('CALORIE').AsInteger;
+
+     //Calcul du temps total de préparation
+     RecipeDbf.Locate('CODE', StarterId, [loCaseInsensitive]);
+     totalDuration := RecipeDbf.FieldByName('PREPARATIO').AsInteger + RecipeDbf.FieldByName('TPS_CUISSO').AsInteger;
+     RecipeDbf.Locate('CODE', MealId, [loCaseInsensitive]);
+     totalDuration := totalDuration + RecipeDbf.FieldByName('PREPARATIO').AsInteger + RecipeDbf.FieldByName('TPS_CUISSO').AsInteger;
+     RecipeDbf.Locate('CODE', DessertId, [loCaseInsensitive]);
+     totalDuration := totalDuration + RecipeDbf.FieldByName('PREPARATIO').AsInteger + RecipeDbf.FieldByName('TPS_CUISSO').AsInteger;
+
+     //Ajout du texte dans la zone d'information
+     CaloriesLabel.Caption := 'Calories : ' + IntToStr(totalCalories) + ' kCal';
+     DurationLabel.Caption := 'Temps total (prép. + cuisson) : ' + IntToStr(totalDuration) + ' min';
+end;
+
 constructor TPlanningDayControl.Create(AOwner: TComponent);
 begin
-     inherited Create(AOwner) ;
+     inherited Create(AOwner);
+
+     StarterId := -1;
+     MealId := -1;
+     DessertId := -1;
 
      RecipeDbf.FilePathFull := GetCurrentDir();
      RecipeDbf.TableLevel := 4;
@@ -59,20 +124,38 @@ begin
      RecipeDbf.Open;
 end;
 
+function TPlanningDayControl.GetStarter : Integer;
+begin
+     GetStarter := StarterId;
+end;
+
 procedure TPlanningDayControl.SetStarter(Id: Integer);
 begin
      //Affichage de l'entrée avec l'ID Id
-     ListView1.Items.Item[0].Caption := GetRecipeName(Id);
+     StarterId := Id;
+     UpdateListView;
+end;
+
+function TPlanningDayControl.GetMeal : Integer;
+begin
+     GetMeal := MealId;
 end;
 
 procedure TPlanningDayControl.SetMeal(Id: Integer);
 begin
-     ListView1.Items.Item[1].Caption := GetRecipeName(Id);
+     MealId := Id;
+     UpdateListView;
+end;
+
+function TPlanningDayControl.GetDessert : Integer;
+begin
+     GetDessert := DessertId;
 end;
 
 procedure TPlanningDayControl.SetDessert(Id: Integer);
 begin
-     ListView1.Items.Item[2].Caption := GetRecipeName(Id);
+     DessertId := Id;
+     UpdateListView;
 end;
 
 end.
