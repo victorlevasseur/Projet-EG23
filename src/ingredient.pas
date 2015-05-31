3321338735332db462ee336adf35d31d714f31e2
victorlevasseur <victorlevasseur52@gmail.com>
2015-06-01 00:02:39 +0200

    Finalisation de l'éditeur d'ingrédients

--- src/ingredient.pas
+++ src/ingredient.pas
@@ -5,20 +5,36 @@ unit Ingredient;
 interface
 
 uses
-  Classes, SysUtils, dbf, db, FileUtil, Forms, Controls, StdCtrls, DBGrids;
+  Classes, SysUtils, dbf, db, FileUtil, Forms, Controls, StdCtrls, DBGrids,
+  Dialogs, DbCtrls, strutils;
 
 type
 
   { TIngredientFrame }
 
   TIngredientFrame = class(TFrame)
+    DBComboBox1: TDBComboBox;
+    DBComboBox2: TDBComboBox;
+    DBComboBox3: TDBComboBox;
+    DBEdit1: TDBEdit;
+    DBEdit2: TDBEdit;
     DBGrid1: TDBGrid;
+    DBText1: TDBText;
     IngrDS: TDataSource;
     IngrDbf: TDbf;
     Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    Label7: TLabel;
+    Label8: TLabel;
     RemoveIngrButton: TButton;
     NewIngrButton: TButton;
     SearchIngrEdit: TEdit;
+    procedure IngrDbfFilterRecord(DataSet: TDataSet; var Accept: Boolean);
+    procedure SearchIngrEditChange(Sender: TObject);
   private
     { private declarations }
   public
@@ -31,6 +47,27 @@ type
 implementation
 
 {$R *.lfm}
+
+procedure TIngredientFrame.SearchIngrEditChange(Sender: TObject);
+begin
+    if SearchIngrEdit.Text = '' then
+    begin
+        IngrDbf.Refresh;
+        IngrDbf.Filtered := False;
+    end
+    else
+    begin
+        IngrDbf.Refresh;
+        IngrDbf.Filtered := True;
+    end;
+end;
+
+procedure TIngredientFrame.IngrDbfFilterRecord(DataSet: TDataSet;
+  var Accept: Boolean);
+begin
+    Accept := AnsiStartsText(SearchIngrEdit.Text,
+        IngrDbf.FieldByName('INTITULE').AsString);
+end;
 
 constructor TIngredientFrame.Create(AOwner: TComponent);
 begin
